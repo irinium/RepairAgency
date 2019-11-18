86d729943f23eabf3652af6e047cc6ac4c076ccb
Irinashvec999 <irinashvec999@gmail.com>
2019-11-18 11:09:53 +0200

    added exception handling and tests to servers, edit master page

--- out/artifacts/final_project_jdbc_war_exploded/WEB-INF/classes/db/03_Users.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `Users`
-    (`user_id`,`email`,`password`,`phone`,`role_id`, `user_name`,`surname`,`address_id`,`appliance_id`)
-VALUES
-    (1,'alex52@gmail.com','alex522','380986543256',2,null,null,null,0),
-    (2,'oleh.vilkov19@gmail.com','190098a8','380989845236',2,null,null,null,0),
-    (3,'anna.lishyna22@gmail.com','yti052','380962564752',3,null,null,null,2),
-    (4,'alex777@gmail.com','alex052','380986573256',3,null,null,null,3),
-    (5,'oleh.vilkov25@gmail.com','190498a8','380989845236',1,'Oleh','Vilkov',1,2),
-    (6,'anna.lishyna2022@gmail.com','yti52','380961564752',1,'Anna','lishyna',2,3);
