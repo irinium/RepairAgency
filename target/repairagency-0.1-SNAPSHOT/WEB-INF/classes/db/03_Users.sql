d7ab6947521f1f1e1c81a2eb4121658830203f5e
Irinashvec999 <irinashvec999@gmail.com>
2019-11-21 20:12:04 +0200

    cleaned code

--- target/repairagency-0.1-SNAPSHOT/WEB-INF/classes/db/03_Users.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `Users`
-    (`user_id`,`email`,`password`,`phone`,`role_id`, `user_name`,`surname`)
-VALUES
-    (1,'admin@gmail.com','YWRtaW5rb3Y=','380986543256',2,'Alex','Vinokurov'), /*adminkov*/
-    (2,'customer@gmail.com','Y3VzdG9tZXI=','380989845236',1,'Oleg','Vilkov'), /*customer*/
-    (3,'master@gmail.com','bWFzdGVyb2s=','380989845236',3,'Oleg','Vilkov'); /*masterok*/
